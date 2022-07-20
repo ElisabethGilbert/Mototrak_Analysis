addd316225df65e1cf8f37ff1003b8da42dbff6c
Mailys31 <107123093+Mailys31@users.noreply.github.com>
2022-07-20 16:50:39 -0400

    Update plot_mean_hit_thresh.m

--- plot_mean_hit_thresh.m
+++ plot_mean_hit_thresh.m
@@ -1,14 +1,31 @@
-function plot_mean_hit_thresh()
+function plot_mean_hit_thresh = plot_mean_hit_thresh ()
+
+prompt = {'Enter number of rat:','Plot title:'};
+dlgtitle = 'Input';
+dims = [1 50];
+answer = inputdlg(prompt)
+
+
+user_val = str2num(answer{1});
+title_val = (answer{2});
+
+for j =1:user_val
 
 folder = uigetdir('C:\MotoTrak Files\', 'Choose Rat');      % go on the forlder and choose rat
 [filepath,name,ext] = fileparts(folder);                                    % take the name of the folder which is the name of the rat
 
-D = dir([folder '\*.mat']);                                                 % take only .mat folder for D
+D = dir([folder '\*.mat']);
 
-numfich = size(D,1);
+numfich(j) = size(D,1);
 
 hit_thresh = [];
-session_trials = nan(numfich,2);
+%session_trials = nan(numfich,2);
+filename = ([folder filesep num2str(name) '_global_stats.mat'])
+
+
+    if exist(filename)
+        numfich = numfich-1    
+    end
 
 for i = 1:numfich
     load(fullfile(D(i).folder,D(i).name));
@@ -17,13 +34,15 @@ for i = 1:numfich
     
     session_trials(i,:) = [i trial_table.Properties.CustomProperties.num_trials];
     
-    mean_hit_thresh_session(i) = (mean(hit_thresh))
+    mean_hit_thresh_session(i) = (mean(hit_thresh));
+    
 end
-
-% figure                                                                      % new figure
-% plot (hit_thresh,'LineWidth',1 )
-% title(name)                                                                 % name of figure
-% xlabel('Number of trials')                                                % names of axes
-% ylabel('Hit thresh (deg)')
-% ylim ([0 80])
-% xlim ([0 3500])
+mean_hit_thresh_rat(j) =(mean(mean_hit_thresh_session));
+end
+ figure                                                                      % new figure
+ plot (mean_hit_thresh_rat,'LineWidth',1 )
+ title(title_val)                                                                 % name of figure
+ xlabel('Number of trials')                                                % names of axes
+ ylabel('Hit thresh (deg)')
+ %ylim ([0 80])
+ %xlim ([0 3500])
